10d69a42cf01770c17e124931fa9a02dd003715a
Tamu <148493182+autamubilismo@users.noreply.github.com>
2026-01-09 12:21:52 -0300

    fixed package json

---
+++ .next/dev/server/chunks/ssr/13b27_autamubilismo__next-internal_server_app__not-found_page_actions_e356f149.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/Users/tamar/Desktop/autamubilismo/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=13b27_autamubilismo__next-internal_server_app__not-found_page_actions_e356f149.js.map
